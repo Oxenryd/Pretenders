60efe881c0d76ec2a7878d6b003d73e558e8b279
MrGameatron <melvinbernshed@gmail.com>
2024-03-16 13:27:55 +0100

    Rotation WIP

--- Assets/Scripts/Animations/HeroAnimation.cs
+++ Assets/Scripts/Animations/HeroAnimation.cs
@@ -5,61 +5,26 @@ using UnityEngine;
 public class HeroAnimation : MonoBehaviour
 {
     private Animator _animator;
-    [SerializeField] private Hero _player;
-    private bool _isMoving = false;
-    private bool _isJumping = false;
-    private bool _isFalling = false;
-    private bool _moveFallCheck = false;
-    private bool _isMovingAfterFall = false;
-    private bool _isGrounded;
+    private ICharacterMovement _characterMovement;
     private string _isJumpingBoolName = "IsJumping";
     private string _isMovingBoolName = "IsMoving";
     private string _isFallingBoolName = "IsFalling";
-    private string _isMoivngAfterFallBoolName = "IsMovingAfterFall";
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
     }
+    private void Start()
+    {
+        _characterMovement = transform.parent.GetComponent<HeroMovement>();
+    }
+
     void Update()
     {
-        _animator.SetBool(_isMovingBoolName, _isMoving);
-        _animator.SetBool(_isJumpingBoolName, _isJumping);
-        _animator.SetBool(_isFallingBoolName, _isFalling);
-        movingAfterFallCheck();
-        _animator.SetBool(_isMoivngAfterFallBoolName, _isMovingAfterFall);
+        _animator.SetBool(_isMovingBoolName, _characterMovement.IsMoving);
+        _animator.SetBool(_isJumpingBoolName, _characterMovement.IsJumping);
+        _animator.SetBool(_isFallingBoolName, _characterMovement.IsFalling);
 
-    }
-    public void SetMoving(bool isMoving)
-    {
-        _isMoving = isMoving;
-    }
-    public void SetJumping(bool isJumping)
-    {
-        _isJumping = isJumping;
-    }
-    public void SetFalling(bool isFalling)
-    {
-        _isFalling = isFalling;
-        if (_isFalling )
-        {
-            _moveFallCheck = true;
-        }
-    }
-    public void SetGrounded(bool isGrounded)
-    {
-        _isGrounded = isGrounded;
-    }
-    private void movingAfterFallCheck()
-    {
-        if (_isMoving && _moveFallCheck && _isGrounded)
-        {
-            _isMovingAfterFall = true;
-            _moveFallCheck = false;
-        }
-        else
-        {
-            _isMovingAfterFall = false;
-        }
+        transform.eulerAngles = _characterMovement.CurrentDirection;
     }
 }
