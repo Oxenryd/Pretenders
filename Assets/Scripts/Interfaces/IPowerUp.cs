646eb9fb5d5096a416322345f9b24645b3d0c46c
HecticSwede <hectorastr@gmail.com>
2024-03-17 16:03:31 +0100

    Continued Development of the Brawler Minigame. Added prototype of random spawning powerups. Added bounce pads.

---
+++ Assets/Scripts/Interfaces/IPowerUp.cs
@@ -0,0 +1,11 @@
+﻿namespace Assets.Scripts.Interfaces
+{
+    internal interface IPowerUp: ICollectable
+    {
+        public float LifeTime { get; set; }
+        public float ActivateTime { get; set; }
+        public bool Expired { get; set; }
+        public void ApplyEffect(); 
+        public void OnEffectEnd();
+    }
+}
