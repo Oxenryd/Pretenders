065ed6fa986d6290471a952a13803a36b90c9279
Henrik <staaf.henrik@gmail.com>
2024-03-22 13:15:31 +0100

    Included some code to transform food when its on the tray

--- Assets/Scripts/Force Feeder/Tray.cs
+++ Assets/Scripts/Force Feeder/Tray.cs
@@ -10,7 +10,7 @@ public class Tray : Grabbable, IRecievable
     [SerializeField] private TransferAlert _transferAlert;
     public TransferAlert TransferAlert
     { get { return _transferAlert; } }
-
+    private Vector3 _itemOffset; 
 
     void Start()
     {
@@ -20,6 +20,24 @@ public class Tray : Grabbable, IRecievable
         var container = GameObject.FindWithTag(GlobalStrings.NAME_UIOVERLAY);
         _transferAlert = GameObject.Instantiate(_transferAlert, container.transform);
         _transferAlert.gameObject.SetActive(false);
+        BoxCollider _boxCollider = GetComponent<BoxCollider>();
+        if (_boxCollider != null)
+        {
+            // Get the bounds of the object
+            Bounds bounds = _boxCollider.bounds;
+
+            // Extract length, width, and height from the bounds
+            float Length = bounds.size.x/transform.localScale.x/2f;
+
+            // Output the dimensions
+            _itemOffset = new Vector3(Length, 0, 0);
+            Debug.LogError(Length);
+        }
+        else
+        {
+            Debug.LogError("Renderer component not found!");
+        }
+
     }
 
 
@@ -28,7 +46,7 @@ public class Tray : Grabbable, IRecievable
         base.Update();
         for(int i =0; i< _heldObjects.Count; i++)
         {
-            _heldObjects[i].transform.position = this.transform.position;
+            _heldObjects[i].transform.position = transform.TransformPoint(_itemOffset);
             _heldObjects[i].transform.rotation = this.transform.rotation;
         }
 
@@ -58,6 +76,7 @@ public class Tray : Grabbable, IRecievable
         }
         return 0;
     }
+
 
 
 }
