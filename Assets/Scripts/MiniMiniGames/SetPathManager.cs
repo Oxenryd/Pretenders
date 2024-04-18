0c8f889a3a86a420fc4b764cc59d3c479d757024
JonathanJakWit <jonathanwj.student@gmail.com>
2024-04-18 14:08:38 +0200

    Added functionality to the lights

--- Assets/Scripts/MiniMiniGames/SetPathManager.cs
+++ Assets/Scripts/MiniMiniGames/SetPathManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class SetPathManager : MonoBehaviour
 {
+    public GameObject headlightPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 lightOneStartPos = new Vector3 (0, 0, 0);
+        Vector3 lightTwoStartPos = new Vector3 (0, 0, 0);
+        Vector3 lightThreeStartPos = new Vector3 (0, 0, 0);
+
+        // Spawn in one/multiple light prefabs
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Randomize the lights next location
+        // Move the lights to the next location
     }
 }
