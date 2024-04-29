aac0a08ed14df5e2571649894995adf38aea156d
HecticSwede <hectorastr@gmail.com>
2024-04-29 15:40:00 +0200

    Some light changes

--- Assets/Scripts/PlatformBrawler/BackgroundMovement.cs
+++ Assets/Scripts/PlatformBrawler/BackgroundMovement.cs
@@ -14,33 +14,38 @@ namespace Assets.Scripts.PlatformBrawler
         float _acceleration;
         Vector3 _targetPosition;
         Vector3 _originalPosition;
-        Vector3 _currentPosition;
+        bool moveRight;
         float _distance = 50;
         void Start()
         {
             _moveTime = UnityEngine.Random.Range(20, 30);
             _timeToMove = new EasyTimer(_moveTime);
-            _currentPosition = gameObject.transform.position;
-            _originalPosition = _currentPosition;
-            _targetPosition = new Vector3
-                (gameObject.transform.position.x + _distance, gameObject.transform.position.y, gameObject.transform.position.z);
-            Debug.Log(_currentPosition);
+            _originalPosition= gameObject.transform.position;
+            
+            moveRight = true;
         }
         void Update()
         {
-            _currentPosition = gameObject.transform.position;
-            if (_timeToMove.Done && _currentPosition != _targetPosition)
+            if (moveRight)
             {
-                _acceleration += 0.0001f;
+                _targetPosition 
+                    = new Vector3(_originalPosition.x + _distance, _originalPosition.y, _originalPosition.z);
+            }
+            else
+            {
+                _targetPosition 
+                    = new Vector3(_originalPosition.x - _distance, _originalPosition.y, _originalPosition.z);
+            }
+            if (_timeToMove.Done)
+            {
+                _acceleration += 0.00001f;
                 gameObject.transform.position =
                     Vector3.Lerp(gameObject.transform.position, _targetPosition, Time.deltaTime * _acceleration);
-            }    
-            if (_timeToMove.Done && _currentPosition == _targetPosition)
-            {
-                _acceleration += 0.0001f;
-                gameObject.transform.position =
-                    Vector3.Lerp(_currentPosition, _originalPosition, Time.deltaTime * _acceleration);
-            }
+                if (gameObject.transform.position == _targetPosition )
+                {
+                    _timeToMove.Reset();
+                }
+            }             
         }
     }
 }
