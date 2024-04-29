f757525999b35e4a00ed78676a572e78681f7271
Oxenryd <oxenryd@gmail.com>
2024-04-29 15:39:47 +0200

    music fix

--- Assets/Scripts/Misc/LobbySceneScript.cs
+++ Assets/Scripts/Misc/LobbySceneScript.cs
@@ -30,7 +30,7 @@ public class LobbySceneScript : MonoBehaviour
         else
             _transition.Value = 1;
         _fadeTimer.Reset();
-        GameManager.Instance.Music.Fadeout(3f);
+        GameManager.Instance.Music.Crossfade(Music.LOBBY, 0, 2f);
 
         //DEBUG DEBUG DEBUG
         //GameManager.Instance.InputManager.Heroes[0].PressedPushButton += testTournamentRando;
