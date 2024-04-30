4819c623fd6f8e05dc5dac65d4f7bf6799efba52
Oxenryd <oxenryd@gmail.com>
2024-04-30 10:19:19 +0200

    commit for henrik

--- Assets/Scripts/Force Feeder/ForceFeederMatchManager.cs
+++ Assets/Scripts/Force Feeder/ForceFeederMatchManager.cs
@@ -1,10 +1,6 @@
-using Newtonsoft.Json.Linq;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.InputSystem.Android;
 
 public class MatchManager : MonoBehaviour
 {
