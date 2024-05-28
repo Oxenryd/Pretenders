c6d7d13996fca6dee100c8e0611370df4a6a2bf0
Oxenryd <oxenryd@gmail.com>
2024-05-28 17:51:58 +0200

    fixed place bomb on throw at x2 bug in BB

--- Assets/Scripts/BombermanScripts/BombSack.cs
+++ Assets/Scripts/BombermanScripts/BombSack.cs
@@ -69,6 +69,8 @@ public class BombSack : MonoBehaviour
 
     private void TrySpawnBomb(object sender, EventArgs e)
     {
+        if (character.IsGrabbing) return;
+
         int activeBombs = 0;
         for (int i = 0; i < bombs.Length; i++)
         {
